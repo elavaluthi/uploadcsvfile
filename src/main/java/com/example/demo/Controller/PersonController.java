9395482e9d31506a7322aca0c2e36e0d566ea2c3
elavaluthi <68210017+elavaluthi@users.noreply.github.com>
2023-03-23 18:04:03 +0530

    Update PersonController.java

--- src/main/java/com/example/demo/Controller/PersonController.java
+++ src/main/java/com/example/demo/Controller/PersonController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.Service.PersonService;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/persons")
 public class PersonController {
